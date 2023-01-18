0420cbf19a2d8527eec30e78839c88be1fe52b4e
Dkingofcode <doladepo128@gmail.com>
2023-01-18 12:00:19 +0000

    created qrcode function

--- app.py
+++ app.py
@@ -3,8 +3,23 @@
 # save the qrcode as an image
 # run the function
 
+import qrcode 
 
+def generate_qrcode(text):
 
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROT_CORRECT_L_,
+        box_size = 10,
+        border = 4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("https://www.davidbeast.com")      
 
 
 
