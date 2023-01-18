8fe8e728ecd24329df5e5a9812be480a10b726e1
Dkingofcode <doladepo128@gmail.com>
2023-01-18 11:24:11 +0000

    steps listed

---
+++ app.py
@@ -0,0 +1,27 @@
+# install all the libraries needed
+# create a function that collects a text and converts it to a qrcode
+# save the qrcode as an image
+# run the function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
